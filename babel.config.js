c158e53d3206fe4478512945a57c87ff6a76a7eb
Soy <kushwahashivanshu350@gmail.com>
2026-04-05 01:45:09 +0530

    Fix Babel SyntaxError and update presets

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
+    ],
     plugins: [
       [
         'module-resolver',
@@ -20,7 +23,6 @@ module.exports = function (api) {
           },
         },
       ],
-      'nativewind/babel',
       'react-native-reanimated/plugin',
     ],
   };
