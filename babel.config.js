c0e4c24f670114a585af7f28ed0ae7154dbb0c2b
Soy <kushwahashivanshu350@gmail.com>
2026-04-05 01:26:29 +0530

    Fix app crash and build optimization

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,26 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './src',
+            '@screens': './src/screens',
+            '@components': './src/components',
+            '@navigation': './src/navigation',
+            '@context': './src/context',
+            '@hooks': './src/hooks',
+            '@api': './src/api',
+            '@utils': './src/utils',
+            '@mytypes': './src/types',
+          },
+        },
+      ],
+      'nativewind/babel',
+      'react-native-reanimated/plugin',
+    ],
   };
 };
